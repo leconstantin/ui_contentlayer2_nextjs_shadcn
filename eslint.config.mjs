4eb35aca0de4e78469e481d8edb06f128e1cf3a4
leconstantin <lcon69184@gmail.com>
2025-01-04 21:17:14 +0200

    Removing errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "react/jsx-key": "off",
+      "tailwindcss/no-custom-classname": "off",
+      "tailwindcss/classnames-order": "error",
+      "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"]
+    }
+  }
 ];
 
 export default eslintConfig;
